0c92b90559df274f76068d703f5a26d8f9e8bf57
Abdul Hannan <abdulhannanhere@gmail.com>
2025-07-10 03:30:56 +0500

    implementing animation using motion

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { assets, cityList } from "@/assets/assets";
 import { useState } from "react";
 import SelectLoc from "./SelectLoc";
 import { useAppContext } from "@/context/Appcontext";
+import { motion } from "motion/react";
 
 const Hero = () => {
   const { pickupDate, setPickupDate, returnDate, setReturnDate, navigate } =
@@ -16,11 +17,24 @@ const Hero = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center">
-      <h1 className="text-4xl md:text-5xl font-semibold">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.8 }}
+      className="h-screen flex flex-col items-center justify-center gap-14 bg-light text-center"
+    >
+      <motion.h1
+        initial={{ y: 50, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.2 }}
+        className="text-4xl md:text-5xl font-semibold"
+      >
         Luxury cars on rent
-      </h1>
-      <form
+      </motion.h1>
+      <motion.form
+        initial={{ y: 50, opacity: 0, scale: 0.95 }}
+        animate={{ y: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
         onSubmit={handleSearch}
         className="flex flex-col md:flex-row items-start md:items-center justify-between p-5 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]"
       >
@@ -67,18 +81,30 @@ const Hero = () => {
               required
             />
           </div>
-          <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary-second hover:bg-primary-dull text-white rounded-full cursor pointer">
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary-second hover:bg-primary-dull text-white rounded-full cursor pointer"
+          >
             <img
               src={assets.search_icon}
               alt="search"
               className="brightness-300"
             />
             Search
-          </button>
+          </motion.button>
         </div>
-      </form>
-      <img src={assets.main_car} alt="car" className="max-h-74" />
-    </div>
+      </motion.form>
+      <motion.img
+        initial={{ y: 100, opacity: 0, }}
+        animate={{ y: 0, opacity: 1, }}
+        transition={{ duration: 0.8, delay: 0.6 }}
+        src={assets.main_car}
+        alt="car"
+        className="max-h-74"
+        loading="lazy"
+      />
+    </motion.div>
   );
 };
 
